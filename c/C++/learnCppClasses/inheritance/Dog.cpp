091d9e37b9e37af331ec581ecc29a622d1153f3b
HackTheOxidation <tomas.hagenau@protonmail.ch>
2023-07-02 07:53:16 +0200

    Implement the chocolate agency algorithm in C++, and clean up old code examples.

--- c/C++/learnCppClasses/inheritance/Dog.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <string>
-#include "Animal.h"
-#include "Dog.h"
-
-using namespace std;
-
-
--- c/C++/overloading/subscript.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <string>
-
-#include "subscript.hpp"
-
-void Stupid::operator[](std::string index) {
-    std::cout << index << std::endl;
-}
-
