142d68fea9aa928e0253d9dfe61962d4a6419bbd
HackTheOxidation <tomas.hagenau@protonmail.ch>
2023-07-20 11:57:14 +0200

    Solve chocolate agency for ADS2

--- c/C++/algorithms/chocolate.cpp
+++ c/C++/algorithms/chocolate.cpp
@@ -1,46 +1,31 @@
+#include <algorithm>
 #include <array>
 #include <functional>
 #include <iostream>
 #include <map>
 #include <optional>
-#include <vector>
 #include <utility>
-
 
 using Coord = std::pair<std::size_t, std::size_t>;
 using Profits = std::map<Coord, int>;
 using ProfitFun = std::function<int(std::size_t, std::size_t)>;
-template<std::size_t N> using Pricelist = std::array<int, N>;
+template<std::size_t N> using Pricelist = const std::array<int, N>;
 
-auto maximum_profit(const Profits &profits) -> std::optional<Coord> {
-  std::optional<std::pair<Coord, int>> max = std::nullopt;
-
-  for (auto [k, v] : profits) {
-    if (!max.has_value()) {
-      max = std::make_pair(k, v);
-      continue;
-    }
-
-    if (max.has_value() && v > max.value().second)
-      max = std::make_pair(k, v);
-  }
-
-  return max ? std::make_optional(max.value().first) : std::nullopt;
+auto maximum_profit(const Profits &profits) -> Coord {
+  auto [k, _] = *std::max_element(profits.begin(), profits.end());
+  return k;
 }
 
 template<std::size_t N>
-auto chocolate_profit(std::size_t x, Pricelist<N> s, Pricelist<N> b) -> std::optional<Coord> {
-  if (x >= N)
-    return std::nullopt;
-
-  ProfitFun profit = [&, s, b](std::size_t i, std::size_t j) {
+auto chocolate_profit(const std::size_t x, Pricelist<N> s, Pricelist<N> b) -> Coord {
+  ProfitFun profit = [&, s, b](const std::size_t i, const std::size_t j) {
     return b[j] - s[i] - (int(j) - int(i)) * 100;
   };
 
   Profits profits;
 
   std::function<void(std::size_t, std::size_t)> recurse =
-    [&, x](std::size_t i, std::size_t j) mutable {
+    [&, x](const std::size_t i, const std::size_t j) mutable {
       if (j == N - 1) {
         profits[std::pair(i, j)] = profit(i, j);
       } else if (i == x) {
@@ -59,17 +44,14 @@ auto chocolate_profit(std::size_t x, Pricelist<N> s, Pricelist<N> b) -> std::opt
 }
 
 auto main() -> int {
-  Pricelist<5> s = {600, 350, 400, 400, 500};
-  Pricelist<5> b = {500, 450, 300, 400, 600};
+  constexpr Pricelist<5> s = {600, 350, 400, 400, 500};
+  constexpr Pricelist<5> b = {500, 450, 300, 400, 600};
+  constexpr std::size_t x = 2;
 
-  auto best = chocolate_profit(2, s, b);
+  static_assert(x < s.size());
 
-  if (best.has_value()) {
-    auto [i, j] = best.value();
-    std::cout << "(" << i << ", " << j << ")\n";
-  } else {
-    std::cout << "Maximum profit could not be determined.\n";
-  }
+  auto [i, j] = chocolate_profit(x, s, b);
+  std::cout << "(" << i << ", " << j << ")\n";
 
   return 0;
 }
